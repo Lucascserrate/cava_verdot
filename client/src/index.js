4ce13cede97417cde84f29e4ac4185388bc15cde
padermo <fabioaeh96@gmail.com>
2023-01-03 18:31:18 -0500

    creando carpetas assets, components, components/App, style, style/style.css, style/normalize.css 03/01 - 18:31

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './style/normalize.css'
+import './style/style.css'
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
