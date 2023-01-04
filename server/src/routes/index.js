5119c05211391f54ed2fd3a327fdf9416580f630
Rene Huaraca <renepersonal931@gmail.com>
2023-01-04 12:07:58 -0500

    ruta products en index 04/01

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
-const router = Router()
-const category = require('./categories.routes.js')
+const router = Router();
+const category = require('./categories.routes.js');
+const product = require('./products.routes.js');
 
 router.use('/categories', category)
+router.use('/productos', product)
 
 module.exports = router
