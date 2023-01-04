2f28f8e33c228258794d68abade221f5607a74cf
Lucascserrate <lucascserrate@gmail.com>
2023-01-03 21:46:22 -0400

    Creando el reducer de redux 03/01

---
+++ client/src/redux/reducer.js
@@ -0,0 +1,14 @@
+import { } from './actions'
+
+const initialState = {
+
+}
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        default: return state
+    }
+}
+
+export default rootReducer
