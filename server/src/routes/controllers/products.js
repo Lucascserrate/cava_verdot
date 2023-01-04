b2b42cd615c89276093365fe8cb83e05139abb95
Ernes <90705607+Erne5@users.noreply.github.com>
2023-01-04 15:23:14 -0300

    correccion en controller products, añadido modelo Category 4/1/23

--- server/src/routes/controllers/products.js
+++ server/src/routes/controllers/products.js
@@ -1,8 +1,12 @@
-const { Drink } = require('../../db');
+const { Drink, Category } = require('../../db');
 
 const getAllProducts = async (req, res) => {
     try {
-        const allDrinks = await Drink.findAll()
+        const allDrinks = await Drink.findAll({
+          include: [{
+            model: Category
+          }]
+        })
         res.status(200).send(allDrinks)
     } catch (error) {
         res.status(404).send({ error: error.message })
