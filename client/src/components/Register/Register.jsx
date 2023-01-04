ff315cb99efeb263bc9a99af0c5a90e4229526b2
padermo <fabioaeh96@gmail.com>
2023-01-04 14:45:07 -0500

    modificando normalize para quitar el margin de los tag h1

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,77 +1,84 @@
-import React, {useState} from 'react';
-import s from './Register.module.css';
-import Alert from '../Alert/Alert';
+import React, { useState } from "react";
+import s from "./Register.module.css";
+import Alert from "../Alert/Alert";
 
 function Register() {
-
   const [user, setUser] = useState({
-    email: '',
-    password: ''
+    email: "",
+    password: "",
   });
 
   const [viewAlert, setViewAlert] = useState();
 
+  // capturamos el value de los inputs
   const HandlerInputs = (e) => {
-    setUser({...user, [e.target.name]: e.target.value});
-  }
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
+  // enviamos la info capturada en los inputs
   const HandlerSubmit = (e) => {
     e.preventDefault();
-    if(!user.email || !user.password){
-      setViewAlert(<Alert type='error' message='Campos vacios.'/>);
-    }else{
+    if (!user.email || !user.password) {
+      setViewAlert(<Alert type="error" message="Campos vacios." />);
+    } else {
       setViewAlert(<Alert type="ok" message="Registro creado exitosamente." />);
       setUser({
-        email: '',
-        password: ''
-      })
+        email: "",
+        password: "",
+      });
     }
-  }
+  };
 
   return (
-    <form className={s.form}>
-      <h1 className={s.form__title}>Register</h1>
-      <div className={s.form__inputs}>
-        <div className={s.form__group}>
-          <label htmlFor="email" className={s.form__lbl}>
-            Email
-          </label>
+    <div className={s.form}>
+      <form className={`${s.form__content} ${s.container}`}>
+        <h1 className={s.form__title}>Register</h1>
+        
+        <div className={s.form__inputs}>
+          <div className={s.form__group}>
+            <label htmlFor="email" className={s.form__lbl}>
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder=""
+              className={s.form__input}
+              value={user.email}
+              name="email"
+              onChange={HandlerInputs}
+            />
+          </div>
+
+          <div className={s.form__group}>
+            <label htmlFor="password" className={s.form__lbl}>
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder=""
+              className={s.form__input}
+              value={user.password}
+              name="password"
+              onChange={HandlerInputs}
+            />
+          </div>
+
           <input
-            id="email"
-            type="email"
-            placeholder=""
-            className={s.form__input}
-            value={user.email}
-            name="email"
-            onChange={HandlerInputs}
+            type="submit"
+            className={s.form__submit}
+            value="Register"
+            onClick={HandlerSubmit}
           />
         </div>
 
-        <div className={s.form__group}>
-          <label htmlFor="password" className={s.form__lbl}>
-            Password
-          </label>
-          <input
-            id="password"
-            type="password"
-            placeholder=""
-            className={s.form__input}
-            value={user.password}
-            name="password"
-            onChange={HandlerInputs}
-          />
+        <div className={s.form__alert}>
+          {viewAlert}
         </div>
-
-        <input
-          type="submit"
-          className={s.form__submit}
-          value="Register"
-          onClick={HandlerSubmit}
-        />
-      </div>
-      {viewAlert}
-    </form>
+      </form>
+    </div>
   );
 }
 
-export default Register
+export default Register;
