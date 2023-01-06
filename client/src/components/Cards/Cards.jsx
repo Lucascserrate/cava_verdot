f62f84748cb71b1ba5bebf6925836abb844d5227
padermo <fabioaeh96@gmail.com>
2023-01-06 18:13:37 -0500

    subiendo cards y card terminadas con estilos, modificado archivo action y reducer para products 06/01 - 18:13

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -3,20 +3,27 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Card from '../Card/Card';
 import s from './Cards.module.css';
+import {getDrinks} from '../../redux/actions'
 
 function Cards() {
+
   const dispatch = useDispatch()
-  const state = useSelector(state => state)
+  const state = useSelector(state => state.drinks.data)
 
   useEffect(()=> {
-    dispatch();
-  },[]);
+    dispatch(getDrinks());
+  },[dispatch]);
   
+
   return (
     <div className={s.cards}>
-      <Card/>
+      {state?.map((e) => (
+        <Link key={e.id} className={s.cards__link}>
+          <Card img={e.image} name={e.name} />
+        </Link>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Cards
--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 // Actions
-// export const GET_DRINKS = 'GET_DRINKS'
+export const GET_DRINKS = 'GET_DRINKS'
 
 // Actions Creators
-// export const getDrinks = () => async dispatch => {}
+export const getDrinks = () => {
+  return async (dispatch) => {
+    let datosApi = await axios.get("http://localhost:3001/products/");
+    dispatch({type: GET_DRINKS, payload: datosApi})
+  }
+}
