255046b984fea54675380b4aa52d384d151df798
padermo <fabioaeh96@gmail.com>
2023-01-07 15:51:10 -0500

    action creators y reducer para categories y filtro category 07/01 - 15:51

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 // Actions
 export const GET_DRINKS = 'GET_DRINKS';
 export const GET_DRINK = 'GET_DRINK';
+export const GET_ALL_CATEGORIES = 'GET_ALL_CATEGORIES';
+export const GET_CATEGORY_PRODUCT = 'GET_CATEGORY_PRODUCT';
 
 // Actions Creators get all products
 export const getDrinks = () => {
@@ -16,5 +18,21 @@ export const getDrinkId = (id) => {
   return async (dispatch) => {
     let datosApi = await axios.get(`http://localhost:3001/products/${id}`);
     dispatch({type:GET_DRINK, payload: datosApi})
+  }
+}
+
+// Action creator get all categories
+export const getAllCategories = () => {
+  return async(dispatch)=>{
+    let datosApi = await axios.get('http://localhost:3001/categories/');
+    dispatch({type: GET_ALL_CATEGORIES, payload: datosApi});
+  }
+}
+
+// Action creator get category product
+export const getCategoryProduct = (category) => {
+  return async(dispatch)=>{
+    let datosApi = await axios.get(`http://localhost:3001/products?category=${category}`);
+    dispatch({type: GET_CATEGORY_PRODUCT, payload: datosApi});
   }
 }
