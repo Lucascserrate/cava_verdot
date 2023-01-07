cb7b41a7b20a277af888166d3504e070051f6bda
padermo <fabioaeh96@gmail.com>
2023-01-07 14:31:41 -0500

    componente details terminado sin estilos 07/01 - 14:31

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Register from '../Register/Register'
 import Cards from '../Cards/Cards'
+import Detail from '../Detail/Detail'
 import { Routes, Route } from 'react-router-dom'
 import Home from '../Home/Home'
 
@@ -8,12 +9,13 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/cards' element={<Cards/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/cards" element={<Cards />} />
+        <Route path='/cards/:id' element={<Detail/>} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App
