6833ffaef52eb31cf5e294b475eb26d316ba8812
MiguelMateoCollado <miguizindex@gmail.com>
2023-01-08 01:52:19 -0300

    Cambios?

--- server/src/routes/countrys.routes.js
+++ server/src/routes/countrys.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 const countries = require("./controllers/countries.js");
+const postCountry = require("./controllers/postCountry.js")
+router.post("/", postCountry);
 router.get("/", countries);
 module.exports = router;
