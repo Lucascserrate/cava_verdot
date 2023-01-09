4a840755ed3de630b898da5c1f0fea26dab1b663
Lucascserrate <lucascserrate@gmail.com>
2023-01-09 18:08:05 -0400

    Modificaciones pequeñas en estilos

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -6,7 +6,7 @@ import Button3 from '../Button3/Button3';
 import ButtonPrimary from '../ButtonPrimary/ButtonPrimary';
 import { useParams } from 'react-router';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { modifyCart } from '../../redux/actions';
 import Footer from '../Footer/Footer'
 import Loader from '../Loader/Loader';
@@ -17,6 +17,7 @@ const Details = () => {
     let { id } = useParams()
     const dispatch = useDispatch()
     const cartAmount = document.getElementById('amount')
+    const cart = useSelector(state => state.cart)
 
     const getDetail = async () => {
         let res = await axios.get(`http://localhost:3001/products/${id}`)
@@ -43,22 +44,22 @@ const Details = () => {
                                 <img className={s.img} src={detail.image ? detail.image : '#'} alt='img' />
                             </div>
                             <div>
-                                <h1 className={s.name}>{detail.name ? detail.name : 'Nombre del Producto'}</h1>
+                                <h1 className={s.name}>{detail?.name}</h1>
                                 <p>⭐⭐⭐⭐⭐</p>
-                                <p className={s.price}>${detail.price ? detail.price : 100}</p>
+                                <p className={s.price}>${detail?.price}</p>
                                 <div>
                                     <span className={s.bold}>Amount:</span>
                                     <input id='amount' className={s.inputAmount} type="number" defaultValue='1' min='1' maxLength='5' />
                                 </div>
                                 <div className={s.buttons}>
                                     <ButtonPrimary handlerAdd={handlerAdd} value='Add' />
-                                    <Button3 value='Pay Now' />
+                                    {cart ? <Button3 value='Pay Now' /> : undefined}
                                 </div>
                                 <p className={s.country}><span className={s.bold}>Country:</span> {detail.country?.country}</p>
                             </div>
                         </div>
                         <br></br>
-                        <div>{detail.description}</div>
+                        <div className={s.description}>{detail.description}</div>
                     </div>
                     : <Loader />
             }
