f1b8ffe3916406701573abeac6e64954d773ebdd
padermo <fabioaeh96@gmail.com>
2023-01-09 14:49:36 -0500

    arreglando ruta de login en app

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/postproduct' element={<FORMIKPostProductForm />} />
         <Route path='/about' element={<About />} />
-        <Route path='/login' element={<About />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </div>
   );
