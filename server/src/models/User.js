0613542bba0f1213e8260a49c2f45a13ee6c12ef
Rene Huaraca <renepersonal931@gmail.com>
2023-01-09 23:40:08 -0500

    validaciones en modelo user 09/01 23:40

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -9,28 +9,54 @@ module.exports = (sequelize) => {
         primaryKey: true,
         autoIncrement: true,
       },
-      name:{
+      name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          isAlpha: true,
+        },
       },
-      surname:{
+      surname: {
         type: DataTypes.STRING,
+        validate: {
+          isAlpha: true,
+        },
+      },
+      age: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min:18,
+        },
       },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          isEmail: true,
+        },
       },
-      passord:{
+      passord: {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      address:{
+      address: {
         type: DataTypes.STRING,
         allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        validate: {
+          isUrl: true,
+        },
+        defaultValue: "https://img2.freepng.es/20180325/wlw/kisspng-computer-icons-user-profile-avatar-5ab7528676bb25.9036280415219636544863.jpg",
+        allowNull:false,
       },
       status: {
         type: DataTypes.BOOLEAN,
-      }
+        defaultValue: true,
+        allowNull:false,
+      },
     },
     {
       createdAt: false,
