ddee95ef1ba70ab13b84cc66c3da13aadc4cd755
padermo <fabioaeh96@gmail.com>
2023-01-12 16:57:14 -0500

    cambiando rutas locales de las actions 12/01 - 16:57

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -21,7 +21,7 @@ const Details = () => {
     const cart = useSelector(state => state.cart)
 
     const getDetail = async () => {
-        let res = await axios.get(`http://localhost:3001/products/${id}`)
+        let res = await axios.get(`https://cavaverdot-production.up.railway.app/products/${id}`)
         setDetail(res?.data[0])
     }
     useEffect(() => {
