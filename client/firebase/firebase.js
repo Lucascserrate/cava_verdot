2279248f572e04def9adf6fae67ad9145b0fb38d
Miguel Angel Mateo Collado <miguizindex@gmail.com>
2023-01-12 00:54:28 -0400

    para probar el deploy otra vez

---
+++ client/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDDC_W0cDPmcfsWACTd_KhCfiHbqpxUIQM",
+    authDomain: "cava-berdot.firebaseapp.com",
+    projectId: "cava-berdot",
+    storageBucket: "cava-berdot.appspot.com",
+    messagingSenderId: "137882118715",
+    appId: "1:137882118715:web:1465981e500904f9964a1e",
+    measurementId: "G-0ER69GWL74"
+};
+const fireApp = initializeApp(firebaseConfig);
