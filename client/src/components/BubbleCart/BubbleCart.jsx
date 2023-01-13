f34ba030ae3f48ef9cbfb1a1a5108300b0c384d2
Lucascserrate <lucascserrate@gmail.com>
2023-01-13 00:46:20 -0400

    cambios antes del localStorage 13/01

--- client/src/components/BubbleCart/BubbleCart.jsx
+++ client/src/components/BubbleCart/BubbleCart.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 
 const BubbleCart = () => {
     const bubbleCart = useSelector(state => state.bubbleCart)
+    /* const bubbleCart = JSON.parse(localStorage.getItem('amount')) */
     return (
         <>
             {
