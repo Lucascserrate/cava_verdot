fb84eeee0d51a321f6f741ae5d7054a588bee3fc
padermo <fabioaeh96@gmail.com>
2023-01-16 14:09:43 -0500

    arreglando estilos del componente register y flow del componente login

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -4,16 +4,22 @@ import s from "./Register.module.css";
 import Alert from "../Alert/Alert";
 
 function Register() {
-  const [timeAlert, setTimeAlert] = useState(false)
+  const [timeAlert, setTimeAlert] = useState(false);
+
+  let getAge = sessionStorage.getItem("age");
 
   return (
     <div className={s.form}>
       <Formik
-        initialValues={{ 
+        initialValues={{
           email: "",
-         password: "",
-
-         }}
+          password: "",
+          name: "",
+          surname: "",
+          age: "",
+          adress: "",
+          image: "",
+        }}
         validate={(values) => {
           let errores = {};
 
@@ -32,13 +38,41 @@ function Register() {
             errores.password = "Por favor ingresa una contraseña";
           }
 
+          //Validaciones NOMBRE
+          if (!values.name) {
+            errores.name = "Por favor ingresa un nombre.";
+          } 
+          // else if (/^[A-ZÑa-zñáéíóúÁÉÍÓÚ'° ]+$/.test(values.name)) {
+          //   errores.name =
+          //     "El nombre solo puede contener letras mayúsculas y minúsculas.";
+          // }
+
+          //Validaciones APELLIDO
+          if (!values.surname) {
+            errores.surname = "Por favor ingresa un apellido.";
+          } 
+          // else if (/^[A-ZÑa-zñáéíóúÁÉÍÓÚ'° ]+$/.test(values.surname)) {
+          //   errores.surname =
+          //     "El apellido solo puede contener letras mayúsculas y minúsculas.";
+          // }
+
+          //Validaciones ADRESS
+          if (!values.adress) {
+            errores.adress = "Por favor ingresa una dirección.";
+          }
+
+          //Validaciones IMAGE
+          if (!values.image) {
+            errores.img = "Por favor ingresa la URL de una imagen.";
+          }
+
           return errores;
         }}
         onSubmit={(values, { resetForm }) => {
-          
           resetForm();
           setTimeAlert(true);
           setTimeout(() => setTimeAlert(false), 5000);
+          console.log(getAge, values);
         }}
       >
         {({ errors }) => (
@@ -46,49 +80,165 @@ function Register() {
             <h1 className={s.form__title}>Register</h1>
 
             <div className={s.form__inputs}>
-              <div className={s.form__group}>
-                <Field
-                  id="email"
-                  type="email"
-                  placeholder=" "
-                  className={s.form__input}
-                  name="email"
-                />
-                <label htmlFor="email" className={s.form__lbl}>
-                  Email:
-                </label>
-                <span className={s.form__bar}></span>
-              </div>
-              <div className={s.form__message}>
-                <ErrorMessage
-                  name="email"
-                  component={() => (
-                    <span className={s.error}>{errors.email}</span>
-                  )}
-                />
-              </div>
-
-              <div className={s.form__group}>
-                <Field
-                  id="password"
-                  type="password"
-                  placeholder=" "
-                  className={s.form__input}
-                  name="password"
-                />
-                <label htmlFor="password" className={s.form__lbl}>
-                  Password:
-                </label>
-                <span className={s.form__bar}></span>
+              <div>
+                <div className={s.form__group}>
+                  <Field
+                    id="name"
+                    type="text"
+                    placeholder=" "
+                    className={s.form__input}
+                    name="name"
+                  />
+                  <label htmlFor="name" className={s.form__lbl}>
+                    Name:
+                  </label>
+                  <span className={s.form__bar}></span>
+                </div>
+                <div className={s.form__message}>
+                  <ErrorMessage
+                    name="name"
+                    component={() => (
+                      <span className={s.error}>{errors.name}</span>
+                    )}
+                  />
+                </div>
               </div>
 
-              <div className={s.form__message}>
-                <ErrorMessage
-                  name="password"
-                  component={() => (
-                    <span className={s.error}>{errors.password}</span>
-                  )}
-                />
+              <div>
+                <div className={s.form__group}>
+                  <Field
+                    id="surname"
+                    type="text"
+                    placeholder=" "
+                    className={s.form__input}
+                    name="surname"
+                  />
+                  <label htmlFor="surname" className={s.form__lbl}>
+                    Surname:
+                  </label>
+                  <span className={s.form__bar}></span>
+                </div>
+                <div className={s.form__message}>
+                  <ErrorMessage
+                    name="surname"
+                    component={() => (
+                      <span className={s.error}>{errors.surname}</span>
+                    )}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className={s.form__group}>
+                  <Field
+                    id="age"
+                    type="number"
+                    placeholder=" "
+                    className={s.form__input}
+                    name="age"
+                  />
+                  <label htmlFor="age" className={s.form__lbl}>
+                    Age: {getAge}
+                  </label>
+                  <span className={s.form__bar}></span>
+                </div>
+              </div>
+
+              <div>
+                <div className={s.form__group}>
+                  <Field
+                    id="adress"
+                    type="text"
+                    placeholder=" "
+                    className={s.form__input}
+                    name="adress"
+                  />
+                  <label htmlFor="adress" className={s.form__lbl}>
+                    Adress:
+                  </label>
+                  <span className={s.form__bar}></span>
+                </div>
+                <div className={s.form__message}>
+                  <ErrorMessage
+                    name="adress"
+                    component={() => (
+                      <span className={s.error}>{errors.adress}</span>
+                    )}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className={s.form__group}>
+                  <Field
+                    id="image"
+                    type="url"
+                    placeholder=" "
+                    className={s.form__input}
+                    name="image"
+                  />
+                  <label htmlFor="image" className={s.form__lbl}>
+                    Image:
+                  </label>
+                  <span className={s.form__bar}></span>
+                </div>
+                <div className={s.form__message}>
+                  <ErrorMessage
+                    name="image"
+                    component={() => (
+                      <span className={s.error}>{errors.image}</span>
+                    )}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className={s.form__group}>
+                  <Field
+                    id="email"
+                    type="email"
+                    placeholder=" "
+                    className={s.form__input}
+                    name="email"
+                  />
+                  <label htmlFor="email" className={s.form__lbl}>
+                    Email:
+                  </label>
+                  <span className={s.form__bar}></span>
+                </div>
+                <div className={s.form__message}>
+                  <ErrorMessage
+                    name="email"
+                    component={() => (
+                      <span className={s.error}>{errors.email}</span>
+                    )}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className={s.form__group}>
+                  <Field
+                    id="password"
+                    type="password"
+                    placeholder=" "
+                    className={s.form__input}
+                    name="password"
+                  />
+                  <label htmlFor="password" className={s.form__lbl}>
+                    Password:
+                  </label>
+                  <span className={s.form__bar}></span>
+                </div>
+
+                <div className={s.form__message}>
+                  <ErrorMessage
+                    name="password"
+                    component={() => (
+                      <span className={s.error}>{errors.password}</span>
+                    )}
+                  />
+                </div>
               </div>
 
               <input
@@ -98,7 +248,11 @@ function Register() {
               />
             </div>
 
-            <div className={s.form__alert}>{timeAlert && (<Alert type="ok" message="Registro creado exitosamente."/>)}</div>
+            <div className={s.form__alert}>
+              {timeAlert && (
+                <Alert type="ok" message="Registro creado exitosamente." />
+              )}
+            </div>
           </Form>
         )}
       </Formik>
