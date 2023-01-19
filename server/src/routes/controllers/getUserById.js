fbf7c5d385644e378f9e3143cfc3778743390e5b
Rene Huaraca <renepersonal931@gmail.com>
2023-01-19 14:33:37 -0500

    guardado para el pull

--- server/src/routes/controllers/getUserById.js
+++ server/src/routes/controllers/getUserById.js
@@ -3,7 +3,12 @@ const { uploadImage } = require("../../Cloudinary/cloudinary.js");
 
 const getUserById = async (req, res) => {
   const { userId } = req.params;
+  const { role, verifyId } = req.query;
   try {
+    console.log();
+    if (role < 3) {
+      if (userId !== verifyId) return res.status(400).send("Not authorized");
+    }
     //cargando roles a la base de datos solo si aún no han sido cargadas
     const userInfo = await User.findByPk(userId, {
       attributes: ["id", "name", "surname", "age", "email", "address", "image"],
