ada0fce220013e3ca84b59d33d9ccf5e3f2e25e2
padermo <fabioaeh96@gmail.com>
2023-01-23 09:31:31 -0500

    subiendo el componente dashboard cliente 80% funcional 23/01 - 09:31

--- client/src/components/DashboardClient/DashboardClient.jsx
+++ client/src/components/DashboardClient/DashboardClient.jsx
@@ -1,23 +1,29 @@
-import React, { useEffect, useState, useRef } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import s from "./DashboardClient.module.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import "./dashboardClient.css";
 import Error from "../Error/Error";
+import NavbarClient from "./NavbarClient/NavbarClient";
 
 function DashboardClient() {
   const stateUser = useSelector((state) => state.user);
 
   const navigate = useNavigate();
 
-  const refEmail = useRef(null);
-  const refAge = useRef(null);
+  // estados que guardan los nuevos valores
+  const [image, setImage] = useState();
+  const [email, setEmail] = useState();
+  const [name, setName] = useState();
+  const [surname, setSurname] = useState();
+  const [age, setAge] = useState();
 
-  const [email, setCurrentEmail] = useState(stateUser?.email);
-  const [age, setCurrentAge] = useState(stateUser?.age);
-
-  const [editEmail, setEditEmail] = useState(false);
-  const [editAge, setEditAge] = useState(false);
+  // estos estados ocultan el modal para luego ser modificados al dar click con las funciones manejadora
+  const [modalEmail, setModalEmail] = useState("hidden");
+  const [modalAge, setModalAge] = useState("hidden");
+  const [modalImage, setModalImage] = useState("hidden");
+  const [modalFullname, setModalFullname] = useState("hidden");
 
   useEffect(() => {
     if (!window.localStorage.getItem("token")) {
@@ -26,105 +32,285 @@ function DashboardClient() {
       }, 2000);
     }
   }, [stateUser, window.localStorage.getItem("token")]);
-  console.log(stateUser);
 
-  const handleEditEmail = () => {
-    setEditEmail(true);
-  };
-
-  const handleEditAge = () => {
-    setEditAge(true);
-  };
-
+  // obtenemos los datos de los inputs
   const handleChangeEmail = (e) => {
-    setCurrentEmail(e.target.value);
+    setEmail(e.target.value);
   };
 
   const handleChangeAge = (e) => {
-    setCurrentAge(e.target.value);
+    setAge(e.target.value);
   };
 
+  const handleChangeImage = async (e) => {
+    const files = e.target.files;
+    console.log(files);
+    const datas = new FormData();
+    datas.append("file", files[0]);
+    datas.append("upload_preset", "CAVA-verdot");
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dcxiks4ku/upload",
+      {
+        method: "POST",
+        body: datas,
+      }
+    );
+    const file = await res.json();
+    setImage(file.secure_url);
+  };
+
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleChangeSurname = (e) => {
+    setSurname(e.target.value);
+  };
+
+  // manejadores del modal email
+  const editEmail = () => {
+    setModalEmail("show");
+  };
+  const closeModalEmail = () => {
+    setModalEmail("hidden");
+  };
   const handleSaveEmail = async () => {
-    const res = await axios.put(`/users/${stateUser?.id}`, { email: email });
-    console.log(email);
-    setEditEmail(false);
+    setModalEmail("hidden");
+    const res = await axios.put(`/users/${stateUser.id}`, { email: email });
     console.log(res);
   };
 
+  // manejadores del modal age
+  const editAge = () => {
+    setModalAge("show");
+  };
+  const closeModalAge = () => {
+    setModalAge("hidden");
+  };
   const handleSaveAge = async () => {
+    setModalAge("hidden");
     const res = await axios.put(`/users/${stateUser.id}`, { age: age });
-    console.log(age);
-    setEditAge(false);
     console.log(res);
   };
+
+  // manejadores del modal image
+  const editImage = () => {
+    setModalImage("show");
+  };
+  const closeModalImage = () => {
+    setModalImage("hidden");
+  };
+  const handleSaveImage = async () => {
+    setModalImage("hidden");
+    const res = await axios.put(`/users/${stateUser.id}`, { image: image });
+    console.log(res);
+  };
+
+  // manejadores del fullname
+  const editFullname = () => {
+    setModalFullname("show")
+  }
+  const closeModalFullname = () => {
+    setModalFullname("hidden")
+  }
+  const handleSaveFullname = async()=>{
+    setModalFullname("hidden");
+    if(name && surname){
+      const res = await axios.put(`/users/${stateUser.id}`, { name: name, surname: surname });
+      console.log(res);
+    }else if(name && !surname){
+      const res = await axios.put(`/users/${stateUser.id}`, { name: name });
+      console.log(res);
+    }else if(surname && !name){
+      const res = await axios.put(`/users/${stateUser.id}`, { surname: surname });
+      console.log(res);
+    }
+  }
 
   return (
     <>
       {window.localStorage.getItem("token") ? (
         <div className={s.dashboard}>
+          <NavbarClient/>
           <div className={`${s.dashboard__content} ${s.container}`}>
             <div className={s.dashboard__cabecera}>
-              <figure className={s.dashboard__picture}>
-                <img
-                  src={stateUser?.image}
-                  alt="image user"
-                  className={s.dashboard__img}
-                />
-              </figure>
-              <h2 className={s.dashboard__fullname}>
-                {stateUser ? stateUser.name + " " + stateUser.surname : ""}
-              </h2>
-            </div>
-
-            <div>
-              <div className={s.dashboard__data}>
-                {stateUser.emailProvider === "local" ? (
-                  editEmail ? (
-                    <>
-                      <input
-                        ref={refEmail}
-                        type="text"
-                        value={email}
-                        onChange={handleChangeEmail}
+              {/* TODO image */}
+              <div>
+                {stateUser?.emailProvider === "local" ? (
+                  <>
+                    <figure
+                      className={s.dashboard__picture}
+                      onClick={editImage}
+                    >
+                      <img
+                        src={stateUser?.image}
+                        alt="image user"
+                        className={s.dashboard__img}
                       />
-                      <button onClick={handleSaveEmail}>
-                        <span class="material-symbols-outlined">done</span>
-                      </button>
-                    </>
-                  ) : (
-                    <>
-                      <button onClick={handleEditEmail}>
-                        <span class="material-symbols-outlined">edit</span>
-                      </button>
-                      {email}
-                    </>
-                  )
+                      <figcaption className={s.dashboard__picoverlay}>
+                        <p>
+                          <span class="material-symbols-outlined">
+                            add_a_photo
+                          </span>
+                        </p>
+                      </figcaption>
+                    </figure>
+                    <div className={`${modalImage}`}>
+                      <div className="modal__content modal__content--img">
+                        <div className="modal__elements modal__elements--img">
+                          <input
+                            type="file"
+                            className="input__edit input__edit--img"
+                            onChange={handleChangeImage}
+                          />
+                          <div className="modal__border">
+                            <img src={image} alt="" className="moda__preview" />
+                          </div>
+                          <button
+                            onClick={handleSaveImage}
+                            className="modal__save"
+                          >
+                            <span className="material-symbols-outlined modal__icon--save">
+                              save
+                            </span>
+                          </button>
+                        </div>
+                        <button
+                          className="modal__close"
+                          onClick={closeModalImage}
+                        >
+                          ✖
+                        </button>
+                      </div>
+                    </div>
+                  </>
                 ) : (
-                  <label>{email}</label>
+                  <>
+                    <figure className={s.dashboard__picture}>
+                      <img
+                        src={stateUser?.image}
+                        alt="image user"
+                        className={s.dashboard__img}
+                      />
+                    </figure>
+                  </>
                 )}
               </div>
 
-              <div className={s.dashboard__data}>
-                {editAge ? (
-                  <>
-                    <input
-                      ref={refAge}
-                      type="text"
-                      value={age}
-                      onChange={handleChangeAge}
-                    />
-                    <button onClick={handleSaveAge}>
-                      <span class="material-symbols-outlined">done</span>
+              {/* TODO: name y surname */}
+              <div className={s.dashboard__fullname}>
+                <h2 className={s.dashboard__name}>{stateUser.name}</h2>
+                <h2 className={s.dashboard__surname}>
+                  {stateUser.surname ? stateUser.surname : ""}
+                </h2>
+                <button onClick={editFullname} className={s.dashboard__edit}>
+                  <span className="material-symbols-outlined">edit</span>
+                </button>
+                <div className={`${modalFullname}`}>
+                  <div className="modal__content modal__content--fullname">
+                    <div className="modal__elements modal__elements--fullname">
+                      <input
+                        type="text"
+                        placeholder="New name..."
+                        className="input__edit"
+                        onChange={handleChangeName}
+                      />
+                      <input
+                        type="text"
+                        placeholder="New surname..."
+                        className="input__edit"
+                        onChange={handleChangeSurname}
+                      />
+                      <button onClick={handleSaveFullname} className="modal__save">
+                        <span className="material-symbols-outlined modal__icon--save">
+                          save
+                        </span>
+                      </button>
+                    </div>
+                    <button className="modal__close" onClick={closeModalFullname}>
+                      ✖
                     </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className={s.dashboard__edit}>
+              <h2>Datos Personales</h2>
+              {/* TODO: email */}
+              <div className={s.dashboard__data}>
+                {stateUser?.emailProvider === "local" ? (
+                  <>
+                    <button onClick={editEmail} className={s.dashboard__edit}>
+                      <span className="material-symbols-outlined">edit</span>
+                    </button>
+                    <p>
+                      Email: <span>{stateUser.email}</span>
+                    </p>
+                    <div className={`${modalEmail}`}>
+                      <div className="modal__content">
+                        <div className="modal__elements">
+                          <input
+                            type="text"
+                            placeholder="New email..."
+                            className="input__edit"
+                            onChange={handleChangeEmail}
+                          />
+                          <button
+                            onClick={handleSaveEmail}
+                            className="modal__save"
+                          >
+                            <span className="material-symbols-outlined modal__icon--save">
+                              save
+                            </span>
+                          </button>
+                        </div>
+                        <button
+                          className="modal__close"
+                          onClick={closeModalEmail}
+                        >
+                          ✖
+                        </button>
+                      </div>
+                    </div>
                   </>
                 ) : (
                   <>
-                    <button onClick={handleEditAge}>
-                      <span class="material-symbols-outlined">edit</span>
-                    </button>
-                    {age}
+                    <p>
+                      Email: <span>{stateUser.email}</span>
+                    </p>
                   </>
                 )}
+              </div>
+
+              {/* TODO: age */}
+              <div className={s.dashboard__data}>
+                <button onClick={editAge} className={s.dashboard__edit}>
+                  <span className="material-symbols-outlined">edit</span>
+                </button>
+                <p>
+                  Age: <span>{stateUser.age}</span>
+                </p>
+                <div className={`${modalAge}`}>
+                  <div className="modal__content">
+                    <div className="modal__elements">
+                      <input
+                        type="text"
+                        placeholder="New age..."
+                        className="input__edit"
+                        onChange={handleChangeAge}
+                      />
+                      <button onClick={handleSaveAge} className="modal__save">
+                        <span className="material-symbols-outlined modal__icon--save">
+                          save
+                        </span>
+                      </button>
+                    </div>
+                    <button className="modal__close" onClick={closeModalAge}>
+                      ✖
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
