3882b29c15524f0f304461318b41470a5dee42ed
Lucascserrate <lucascserrate@gmail.com>
2023-01-24 15:08:55 -0400

    stash

--- client/src/components/Rating/Ratings.jsx
+++ client/src/components/Rating/Ratings.jsx
@@ -4,9 +4,16 @@ import { Rating } from '@mui/material'
 import { useState } from 'react';
 import img from "../../assets/perfil.png";
 import { parseJwt } from "../../functions/parseTokenJwt";
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const Ratings = ({ handlePostReview, value, setValue }) => {
+const Ratings = ({ id }) => {
+    const [value, setValue] = useState({
+        punctuation: 0,
+        review: ''
+    })
 
+    const userId = useSelector(state => state.user)
     const [decodingToken, setDecodingToken] = useState()
 
     const getToken = window.localStorage.getItem("token");
@@ -14,7 +21,7 @@ const Ratings = ({ handlePostReview, value, setValue }) => {
     const handleChangeRate = (e) => {
         setValue({
             ...value,
-            rate: parseInt(e.target.value)
+            punctuation: parseInt(e.target.value)
         })
     }
     const handleChangeReview = (e) => {
@@ -24,12 +31,18 @@ const Ratings = ({ handlePostReview, value, setValue }) => {
         })
     }
 
+    const handlePostReview = async () => {
+        let a = await axios.post(`/products/review?userId=${userId.id}&drinkId=${id}`, value)
+        console.log(a);
+    }
+
+
     useEffect(() => {
         if (getToken) {
             setDecodingToken(parseJwt(getToken))
         }
     }, [])
-    console.log(value);
+    console.log(id);
     return (
         <div className={s.container}>
             <div className={s.box}>
@@ -39,12 +52,12 @@ const Ratings = ({ handlePostReview, value, setValue }) => {
                         <p>Rate</p>
                         <Rating
                             name="simple-controlled"
-                            value={value?.rate}
+                            value={value?.punctuation}
                             onChange={handleChangeRate}
                         />
                     </div>
                     {
-                        value?.rate
+                        value?.punctuation
                             ? <div>
                                 <p>Review</p>
                                 <div className={s.reviewBox}>
