22ab3583b29e9f358634e7d00303e4aaa36d3e08
Miguel Angel Mateo Collado <miguizindex@gmail.com>
2023-01-25 13:43:22 -0400

    Ajuste en el diseno de los filtros

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const { server } = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const PORT = process.env.PORT || 3001;
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`);
   });
