1e90ed58136456e2f8d4dd6c9c600e73db75b475
Paul Esperon <paul.esperonarg@hotmail.com>
2023-01-26 15:16:15 -0300

    cambio de db a dply

--- server/src/db.js
+++ server/src/db.js
@@ -5,13 +5,17 @@ const path = require("path");
 
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
+
+
 const sequelize = new Sequelize(
-	`postgresql://postgres:9y7mV3RuLOJGH6kKslNz@containers-us-west-40.railway.app:7277/railway`,
+	`postgresql://postgres:kpSBAKrmRQHjfEsM4XDl@containers-us-west-123.railway.app:6728/railway`,
 	{
 		logging: false,
 		native: false,
 	},
 );
+
+
 
 
 //  const sequelize = new Sequelize(
